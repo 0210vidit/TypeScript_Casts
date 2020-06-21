e9f876137b3e9bf3cdd8fba8715a66789f9033a5
Vidit Shah <0210vidit@gmail.com>
2020-06-22 04:48:09 +0530

    Create function.ts

---
+++ phase1/features/annotations/function.ts
@@ -0,0 +1,13 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = function (message: string): void {};
