f4aa4c4ae81124ec816d1928486824f4c1d4e337
Vidit Shah <0210vidit@gmail.com>
2020-06-22 02:38:49 +0530

    t3

--- phase1/fetchjson/index.js
+++ phase1/fetchjson/index.js
@@ -2,6 +2,10 @@
 exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
-axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
+axios_1["default"].get(url).then(function (response) {
+    var todo = response.data;
+    var ID = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log(ID + title + completed);
 });
--- phase1/fetchjson/index.ts
+++ phase1/fetchjson/index.ts
@@ -2,6 +2,17 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((res) => {
-  console.log(res.data);
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(ID + title + completed);
 });
