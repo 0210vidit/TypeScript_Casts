3df10104cd1aaf56cded272f626debefee9a7165
Vidit Shah <0210vidit@gmail.com>
2020-06-22 15:50:21 +0530

    array

---
+++ phase1/features/annotations/arrays.ts
@@ -0,0 +1,14 @@
+const carMakers: string[] = ['ford', 'toyota', 'Tesla'];
+const spaceMakers: string[] = [];
+
+const dates = [new Date()];
+
+const carsByMake: string[][] = [['f150'], ['Corolla'], ['camaro']];
+
+const carsss = carMakers[0];
+
+//Mutliple type of arrays:
+
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
+importantDates.push('dasdas');
+importantDates.push(new Date());
